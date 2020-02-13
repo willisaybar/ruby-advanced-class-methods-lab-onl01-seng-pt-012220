0d391f597a7c604d3d6247251b0755cbf0fafb25
Willis Aybar <willisaybar@gmail.com>
2020-02-13 02:28:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
-class Song
+lass Song
   attr_accessor :name, :artist_name
   @@all = []
 
@@ -10,8 +10,25 @@ class Song
     self.class.all << self
   end
 
-  def song.create
-    @@all << song 
+  def self.create
+    song = self.new
+    #@@all << song
+    song
   end
 
-end
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name}
+  end
