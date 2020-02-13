90c80830d46e7852a00c147ffb450f7816480fb3
Willis Aybar <willisaybar@gmail.com>
2020-02-13 02:36:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,4 +33,31 @@ class Song
     @@all.find{|x| x.name == name}
   end
 
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical()
+    @@all.sort_by{|x| x.name}
+  end
+
+  def self.new_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
+
+  def self.destroy_all()
+    @@all.clear
+  end
+
 end
